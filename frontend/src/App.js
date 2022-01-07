972c3782ec0cce4c5a706171ffcb83eee0f4022c
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-01-07 20:16:36 +0600

    Added my project

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import FilesUploadComponent from './components/files-upload-component';
+
+function App() {
+  return (
+    <div className="App">
+      <FilesUploadComponent  />
+    </div>
+  );
+}
+
+export default App;
